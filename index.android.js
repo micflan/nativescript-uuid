371f1f0aeee211b8c8f2901f473db2d3aa842edd
Qanah <i.qanah@gmail.com>
2018-05-24 14:06:30 +0300

    Update index.android.js

--- index.android.js
+++ index.android.js
@@ -1,4 +1,4 @@
-var device = require('../tns-core-modules/platform/platform');
+var device = require('tns-core-modules/platform/platform');
 
 function getUUID() {
   return device ? device.uuid : "";
